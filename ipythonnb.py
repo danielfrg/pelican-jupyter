593ba4e8bd5770195367e0bface7595f16aa8221
Daniel Rodriguez <drodriguez@oculus360.us>
2014-02-16 22:21:45 -0600

    General refactoring to dynamically get the summary of the post

--- ipythonnb.py
+++ ipythonnb.py
@@ -1,19 +1,9 @@
 import os
 from pelican import signals
-
-try:
-    from pelican.readers import BaseReader  # new Pelican API
-except ImportError:
-    from pelican.readers import Reader as BaseReader
-
-try:
-    from pelican.readers import EXTENSIONS  # old Pelican API
-except ImportError:
-    EXTENSIONS = None
+from pelican.readers import MarkdownReader, HTMLReader, BaseReader
 
 try:
     import json
-    import markdown
 
     from IPython.config import Config
     from IPython.nbconvert.exporters import HTMLExporter
@@ -23,6 +13,14 @@ try:
 except Exception as e:
     IPython = False
     raise e
+
+
+# General settings, see add_reader at the end
+
+settings = {}
+
+# Strip HTML tags, for summary creation
+
 
 from html.parser import HTMLParser
 
@@ -45,6 +43,8 @@ def strip_tags(html):
     s.feed(html)
     return s.get_data()
 
+
+# Fix CSS
 
 CUSTOM_CSS = '''
 <style type="text/css">
@@ -95,16 +95,54 @@ table.dataframe th, td {
 
 
 def custom_highlighter(source, language='ipython'):
-    '''
+    """
     Makes the syntax highliting from pygments have prefix(`highlight-ipynb`)
     So it does not break the themes pygments
 
     It modifies both the css and html
-    '''
+    """
     formatter = HtmlFormatter(cssclass='highlight-ipynb')
     output = _pygment_highlight(source, formatter, language)
     output = output.replace('<pre>', '<pre class="ipynb">')
     return output
+
+
+class MyHTMLParser(HTMLReader._HTMLParser):
+    """
+    Extends Pelican HTMLReader._HTMLParser by including the summary of the content
+    based on settings['SUMMARY_MAX_LENGTH'].
+    Also stops the summary if founds any div containing ipython notebook code cells
+
+    This is needed in order to generate valid HTML for the summary, because a simple split
+    breaks the html generating errors on the theme.
+    The downside is that the summary lenght is not exactly the specified, it includes
+    complete div/p/li/etc tags.
+    """
+    def __init__(self, settings, filename):
+        super(MyHTMLParser, self).__init__(settings, filename)
+
+        self.summary = None
+
+    def handle_starttag(self, tag, attrs):
+        super(MyHTMLParser, self).handle_starttag(tag, attrs)
+
+        if self.summary is None:
+            # Check for any ipython cell tags
+            # eg: <div class="cell border-box-sizing code_cell vbox">
+            for k, v in attrs:
+                if k == "class":
+                    ipython_classes = ['cell', 'code_cell']
+                    classes = v.split(' ')
+                    if any(class_ in ipython_classes for class_ in classes):
+                        self.summary = self._data_buffer + '...'
+
+    def handle_endtag(self, tag):
+        super(MyHTMLParser, self).handle_endtag(tag)
+
+        if self.summary is None:
+            self.wordcount = len(strip_tags(self._data_buffer).split(" "))
+            if self.wordcount > self.settings['SUMMARY_MAX_LENGTH']:
+                self.summary = self._data_buffer + '...'
 
 
 class iPythonNB(BaseReader):
@@ -112,60 +150,68 @@ class iPythonNB(BaseReader):
     file_extensions = ['ipynb']
 
     def read(self, filepath):
+        metadata = {}
+
+        # Files
         filedir = os.path.dirname(filepath)
         filename = os.path.basename(filepath)
-
-        _metadata = {}
-        # See if metadata file exists metadata
         metadata_filename = filename.split('.')[0] + '.ipynb-meta'
         metadata_filepath = os.path.join(filedir, metadata_filename)
+
+        # Load metadata
         if os.path.exists(metadata_filepath):
-            with open(metadata_filepath, 'r') as metadata_file:
-                content = metadata_file.read()
-                metadata_file = open(metadata_filepath)
-                md = markdown.Markdown(extensions=['meta'])
-                md.convert(content)
-                _metadata = md.Meta
-
-            for key, value in _metadata.items():
-                _metadata[key] = value[0]
+            # Metadata is on a external file, process using Pelican MD Reader
+            md_reader = MarkdownReader(self.settings)
+            _content, metadata = md_reader.read(metadata_filepath)
         else:
-            # Try to load metadata from inside ipython nb
+            # Load metadata from ipython notebook file
             ipynb_file = open(filepath)
-            _metadata = json.load(ipynb_file)['metadata']
+            metadata = json.load(ipynb_file)['metadata']
 
-        metadata = {}
-        for key, value in _metadata.items():
-            key = key.lower()
-            metadata[key] = self.process_metadata(key, value)
-        metadata['ipython'] = True
+            # Fix metadata to pelican standars
+            for key, value in metadata.items():
+                del metadata[key]
+                key = key.lower()
+                metadata[key] = self.process_metadata(key, value)
+            metadata['ipython'] = True
 
-        # Converting ipythonnb to html
-        config = Config({'CSSHTMLHeaderTransformer': {'enabled': True, 'highlight_class': '.highlight-ipynb'}})
-        exporter = HTMLExporter(config=config, template_file='basic', filters={'highlight2html': custom_highlighter})
+        # Convert ipython notebook to html
+        config = Config({'CSSHTMLHeaderTransformer': {'enabled': True,
+                         'highlight_class': '.highlight-ipynb'}})
+        exporter = HTMLExporter(config=config, template_file='basic',
+                                filters={'highlight2html': custom_highlighter})
 
-        body, info = exporter.from_filename(filepath)
-        metadata['summary'] = ' '.join(strip_tags(body).split(" ")[:50]) + '...'
+        content, info = exporter.from_filename(filepath)
 
-        def filter_tags(s):
-            l = s.split('\n')
+        # Process using Pelican HTMLReader
+        content = '<body>{0}</body>'.format(content)  # So Pelican HTMLReader works
+        parser = MyHTMLParser(self.settings, filename)
+        parser.feed(content)
+        parser.close()
+        body = parser.body
+        summary = parser.summary
+
+        metadata['summary'] = summary
+
+        # Remove some CSS styles, so it doesn't break the themes.
+        def filter_tags(style_text):
+            style_list = style_text.split('\n')
             exclude = ['p', 'h1', 'h2', 'h3', 'h4', 'h5', 'h6', 'a', 'ul', 'ol', '.rendered_html', '@media']
-            l = [i for i in l if len(list(filter(i.startswith, exclude))) == 0]
-            ans = '\n'.join(l)
-            return STYLE_TAG.format(ans)
+            style_list = [i for i in style_list if len(list(filter(i.startswith, exclude))) == 0]
+            ans = '\n'.join(style_list)
+            return '<style type=\"text/css\">{0}</style>'.format(ans)
 
-        STYLE_TAG = '<style type=\"text/css\">{0}</style>'
         css = '\n'.join(filter_tags(css) for css in info['inlining']['css'])
         css = css + CUSTOM_CSS
         body = css + body
+
         return body, metadata
 
 
 def add_reader(arg):
-    if EXTENSIONS is None:  # new pelican API:
-        arg.settings['READERS']['ipynb'] = iPythonNB
-    else:
-        EXTENSIONS['ipynb'] = iPythonNB
+    global settings
+    arg.settings['READERS']['ipynb'] = iPythonNB
+    settings = arg.settings
 
 
 def register():
