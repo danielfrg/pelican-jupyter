3a19ec0842236b468822f2d82982c10292696e3f
admin <kondosatoru@test.local>
2016-03-03 23:16:04 +0900

    edit for python 2.X

--- core.py
+++ core.py
@@ -82,7 +82,7 @@ def get_html_from_filepath(filepath):
     content, info = exporter.from_filename(filepath)
 
     if BeautifulSoup:
-        soup = BeautifulSoup(content)
+        soup = BeautifulSoup(content,"html.parser")
         for i in soup.findAll("div", {"class" : "input"}):
             if i.findChildren()[1].find(text='#ignore') is not None:
                 i.extract()
