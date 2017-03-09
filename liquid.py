1e52ce679a4922cc4307f8b6e7e9da8104db27f5
Jake Vanderplas <jakevdp@gmail.com>
2017-03-09 15:17:47 -0800

    ENH: allow cells[start:end] in liquid (#60)

--- liquid.py
+++ liquid.py
@@ -8,8 +8,14 @@ from liquid_tags.mdx_liquid_tags import LiquidTags
 from .core import get_html_from_filepath, fix_css
 
 
-SYNTAX = "{% notebook ~/absolute/path/to/notebook.ipynb %}"
-FORMAT = re.compile(r"""^(\s+)?(?P<src>\S+)(\s+)?((cells\[)(?P<start>-?[0-9]*):(?P<end>-?[0-9]*)(\]))?(\s+)?((language\[)(?P<language>-?[a-z0-9\+\-]*)(\]))?(\s+)?$""")
+SYNTAX = "{% notebook ~/absolute/path/to/notebook.ipynb [cells[start:end]] %}"
+FORMAT = re.compile(r"""
+^(\s+)?                                                # whitespace
+(?P<src>\S+)                                           # source path
+(\s+)?                                                 # whitespace
+((cells\[)(?P<start>-?[0-9]*):(?P<end>-?[0-9]*)(\]))?  # optional cells
+(\s+)?$                                                # whitespace
+""", re.VERBOSE)
 
 @LiquidTags.register('notebook')
 def notebook(preprocessor, tag, markup):
@@ -19,14 +25,16 @@ def notebook(preprocessor, tag, markup):
         src = argdict['src']
         start = argdict['start']
         end = argdict['end']
-        language = argdict['language']
     else:
         raise ValueError("Error processing input, "
                          "expected syntax: {0}".format(SYNTAX))
 
+    start = int(start) if start else 0
+    end = int(end) if end else None
+
     # nb_dir =  preprocessor.configs.getConfig('NOTEBOOK_DIR')
     nb_path = os.path.join('content', src)
-    content, info = get_html_from_filepath(nb_path)
+    content, info = get_html_from_filepath(nb_path, start=start, end=end)
     ignore_css = preprocessor.configs.getConfig('IPYNB_IGNORE_CSS', False)
     content = fix_css(content, info, ignore_css=ignore_css)
     content = preprocessor.configs.htmlStash.store(content, safe=True)
