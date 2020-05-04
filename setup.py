fdaeb60c981ebc1a24421c67c02c07a03d785e4f
Felipe S. S. Schneider <schneider.felipe.5@gmail.com>
2020-05-04 10:46:34 -0300

    Add dependencies

--- setup.py
+++ setup.py
@@ -12,4 +12,11 @@ setup(
     author="Daniel Rodriguez",
     url="https://github.com/danielfrg/pelican-ipynb",
     packages=find_packages(),
+    install_requires=[
+        "ipython>=4.0",
+        "jupyter>=1.0",
+        "Markdown>=2.6.1",
+        "nbconvert>=4.0",
+        "pelican>=3.5",
+    ],
 )
